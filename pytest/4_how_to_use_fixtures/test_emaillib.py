29fa9e986fded4924e73c0c3a70e74420a5d58c5
yudavid42 <david06@42maru.ai>
2024-03-29 10:34:22 +0900

    pytest/4_how_to_use_fixtures

---
+++ pytest/4_how_to_use_fixtures/test_emaillib.py
@@ -0,0 +1,31 @@
+from emaillib import Email, MailAdminClient
+
+import pytest
+
+
+@pytest.fixture
+def mail_admin():
+    return MailAdminClient()
+
+
+@pytest.fixture
+def sending_user(mail_admin):
+    user = mail_admin.create_user()
+    yield user
+    print('sending_user teardown!')
+    mail_admin.delete_user(user)
+
+
+@pytest.fixture
+def receiving_user(mail_admin):
+    user = mail_admin.create_user()
+    yield user
+    print('receiving_user teardown!')
+    user.clear_mailbox()
+    mail_admin.delete_user(user)
+
+
+def test_email_received(sending_user, receiving_user):
+    email = Email(subject="Hey!", body="How's it going?")
+    sending_user.send_email(email, receiving_user)
+    assert email in receiving_user.inbox
